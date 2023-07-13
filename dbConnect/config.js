70112a43c86ee5de4af20fc192c1b9d145763afd
Rinkesh375 <rinkeshujjwal16@gmail.com>
2023-07-13 17:00:42 +0530

    post management api

--- dbConnect/config.js
+++ dbConnect/config.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://rinkeshujjwal16:rinkesh@todocrud.arwclpw.mongodb.net/todo');
+mongoose.connect('mongodb+srv://rinkeshujjwal16:rinkesh@todocrud.arwclpw.mongodb.net/?retryWrites=true&w=majority');
